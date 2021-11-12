e076a530d7d496cd95fe514575d2d885351fb19c
Abhijeet <94052679+iamTheAvyzit@users.noreply.github.com>
2021-11-12 01:48:35 -0500

    Adding App.js and Data.js
    it has all functional components. No HOC's involved. It does have a Data.js file that returns an array of Objects, this list contains all the user details that you want to display. And  a button to clear the list.

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import Data from './Data'
+import List from './List'
+
+function App() {
+ 
+  const [people, setPeople] = useState(Data)
+  
+  return (
+    <main>
+      <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
+      <button onClick={()=> setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
